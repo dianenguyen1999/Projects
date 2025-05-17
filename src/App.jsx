4d83fbe8924d28e05dcc604e6caa461e37463e9b
Diane <dianenguyen9521175@gmail.com>
2025-05-16 23:06:28 -0600

    home page edit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { LoadingScreen } from './components/LoadingScreen'
 import { Navbar } from './components/Navbar';
 import { MobileMenu } from './components/MobileMenu';
+import { Home } from './components/sections/Home';
 import "./index.css"
 
 function App() {
---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+    return ( <section id="home" className="min-h-screen flex items-center justify-center relative">
+        <div className="text-center z-10 px-4">
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-red-400 to-white-600 leading-right">
+                Hi, I'm Diane and this is my portfolio ♥
+            </h1>
+        </div>
+     </section>
+    );
+};
