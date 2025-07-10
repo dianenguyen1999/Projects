741b8732f8bd5f88a527f541370ead7622560648
Diane <dianenguyen9521175@gmail.com>
2025-07-10 17:21:25 -0600

    final project

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,3 +1,5 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
 export const About = () => {
 
     const frontendSkills = [
@@ -20,6 +22,7 @@ export const About = () => {
       id="about" 
       className="min-h-screen flex items-center justify-center py-20"
     >
+        <RevealOnScroll>
         <div className="max-w-3xl mx-auto px-4">
             <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-purple-400 bg-clip-text text-transparent text-center"> 
                 About Me
@@ -93,6 +96,7 @@ export const About = () => {
               </div>
             </div>
         </div>
+        </RevealOnScroll>
     </section>
   )
 }
